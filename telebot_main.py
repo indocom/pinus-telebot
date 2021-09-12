52a912d302b4fa0b788b5045361693e6745d636a
Albert Sutiono <albertsutz123@gmail.com>
2021-09-13 02:25:59 +0800

    Initial telebot python script, taken from tutorial

---
+++ telebot_main.py
@@ -0,0 +1,44 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+#logging information
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+#Starting our bot
+#Initialize updator and dispatcher
+updater = Updater(token='TOKEN', use_context=True)
+dispatcher = updater.dispatcher
+
+
+#List of all of our functions
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+#List of handlers
+start_handler = CommandHandler('start', start)
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+caps_handler = CommandHandler('caps', caps)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+
+#Adding handlers to dispatcher
+#Order matters
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(unknown_handler)
+
+updater.start_polling()
