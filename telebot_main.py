223ef7ce1eafc3d3c65ceb15db697de50312c1b8
Albert Sutiono <albertsutz123@gmail.com>
2021-09-14 20:32:11 +0800

    Added Functionality: /repo to list all repo inside indocom

--- telebot_main.py
+++ telebot_main.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import requests
 
 #logging information
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -26,19 +27,36 @@ def caps(update, context):
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
+def repo_list(update, context):
+    url = "https://api.github.com/users/indocom/repos"
+    response = requests.get(url)
+    json = response.json()
 
-#List of handlers
+    repo_text = ("Hi, here is the list of PINUS Repositories: \n")
+    for i in json:
+        repo_text += ("- " + i["name"] + " : " + i["svn_url"] + "\n")
+    
+    context.bot.send_message(chat_id=update.effective_chat.id, text=repo_text)
+
+
+#List of Command Handlers
 start_handler = CommandHandler('start', start)
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 caps_handler = CommandHandler('caps', caps)
+repo_list_handler = CommandHandler('repo', repo_list)
+
+#List of Message Handlers
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 unknown_handler = MessageHandler(Filters.command, unknown)
+
 
 
 #Adding handlers to dispatcher
 #Order matters
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(repo_list_handler)
+
+dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(unknown_handler)
 
 updater.start_polling()
