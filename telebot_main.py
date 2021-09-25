bd41b1a092408e5d879fa65e79863a9cf5d1accb
Albert Sutiono <albertsutz123@gmail.com>
2021-09-26 03:16:34 +0800

    Added Job queue to repeteadly give 1 person PR updates

--- telebot_main.py
+++ telebot_main.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import requests
+import datetime
 
 #logging information
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -17,11 +18,6 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 def unknown(update, context):
@@ -32,31 +28,57 @@ def repo_list(update, context):
     response = requests.get(url)
     json = response.json()
 
-    repo_text = ("Hi, here is the list of PINUS Repositories: \n")
+    reply_text = ("Hi, here is the list of PINUS Repositories: \n")
     for i in json:
-        repo_text += ("- " + i["name"] + " : " + i["svn_url"] + "\n")
+        reply_text += ("- " + i["name"] + " : " + i["svn_url"] + "\n")
     
-    context.bot.send_message(chat_id=update.effective_chat.id, text=repo_text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+def pull_request_logic(context):
+    url = "https://api.github.com/repos/indocom/pinus-client/pulls"
+    response = requests.get(url)
+    json = response.json()
+
+    top_5 = json[0:5]
+    new_pulls = []
+
+    for i in top_5:
+        create_time = datetime.datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        time_difference = (datetime.datetime.now() - create_time).total_seconds()
+        if time_difference < 300 :
+            new_pulls.append(i)
+        
+    reply_text = ("Hi, here is the list of new Pull Request for pinus-client: \n")
+    for i in new_pulls:
+        reply_text += ("- " + i["title"] + " : " + i["html_url"] + "\n")
+    
+    if len(new_pulls) > 0:
+        context.bot.send_message(chat_id=context.job.context, text=reply_text)
+
+def new_pull_request(update, context):
+    text = "Giving you Pull Request updates of pinus-telebot every 5 minutes"
+    context.bot.send_message(chat_id=update.message.chat_id, text=text)
+
+    context.job_queue.run_repeating(pull_request_logic, interval = 10, first = 1, context=update.message.chat_id)
+
+
+
 
 
 #List of Command Handlers
 start_handler = CommandHandler('start', start)
-caps_handler = CommandHandler('caps', caps)
 repo_list_handler = CommandHandler('repo', repo_list)
+new_pull_request_handler = CommandHandler('new_pull_request', new_pull_request)
 
 #List of Message Handlers
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 unknown_handler = MessageHandler(Filters.command, unknown)
-
-
 
 #Adding handlers to dispatcher
 #Order matters
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(caps_handler)
 dispatcher.add_handler(repo_list_handler)
+dispatcher.add_handler(new_pull_request_handler)
 
-dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(unknown_handler)
 
 updater.start_polling()
