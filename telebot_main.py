367e7b4847191cf30df6eb0dd1c4bb77fbde0c3a
nixonwidjaja <69686938+nixonwidjaja@users.noreply.github.com>
2021-10-24 21:11:43 +0800

    Add event

--- telebot_main.py
+++ telebot_main.py
@@ -12,7 +12,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-#from csv_handler import *
+from csv_handler import *
 
 BOT_API_TOKEN = os.environ.get('BOT_API_TOKEN')
 GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
