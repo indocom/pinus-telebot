800393887e4c04b841437724ae7413302111092b
nixonwidjaja <69686938+nixonwidjaja@users.noreply.github.com>
2021-10-24 20:01:50 +0800

    Add event

--- telebot_main.py
+++ telebot_main.py
@@ -12,12 +12,13 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from csv_handler import *
+#from csv_handler import *
 
-BOT_API_TOKEN = ""
+BOT_API_TOKEN = os.environ.get('BOT_API_TOKEN')
+GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
 PORT = int(os.environ.get('PORT', 8443))
 
-chat_ids = []
+# chat_ids = []
 
 #logging information
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -26,17 +27,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 #Starting our bot
 #Initialize updator and dispatcher
-updater = Updater(token= BOT_API_TOKEN, use_context=True)
+updater = Updater(token=BOT_API_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
 #List of all of our functions
 def start(update, context):
-    
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm KawaiiBot, use /list to show list of available commands and /help to know informations about the bot")
 
 def list(update, context):
-    keyboard = keyboard = [['/subscribe', '/help'], ['/status', 'new_pull_request']]
+    keyboard = keyboard = [['/help'], ['/status', '/repo'], ['/new_pull_request'], ['/getevents', '/addevent']]
 
     reply_markup = ReplyKeyboardMarkup(keyboard,
                                        one_time_keyboard=True,
@@ -47,8 +47,10 @@ def list(update, context):
 def help_func(update, context):
     text = ("Hi, here is a list of commands\n\n")
     text += ("/repo:  Get the list of all repositories inside github.com/indocom\n\n")
-    text += ("/subscribe: Subscribe to Pinus-telebot Pull Requests Notification\n\n")
-    text += ("/status: Check whether you are subscribed or not")
+    text += ("/status: Get the list of all repositories that you have subscribed\n\n")
+    text += ("/add_repo <repo link>: Subscribe to a particular repo\n\n")
+    text += ("/getevents: Get the upcoming 10 events\n\n")
+    text += ("/addevent <name> <date> <time> <desc>: Add an event\n\n")
     context.bot.send_message(chat_id=update.effective_chat.id, text = text)
 
 def unknown(update, context):
@@ -56,84 +58,115 @@ def unknown(update, context):
 
 def repo_list(update, context):
     url = "https://api.github.com/users/indocom/repos"
-    response = requests.get(url)
-    json = response.json()
-
-    reply_text = ("Hi, here is the list of PINUS Repositories: \n")
-    for i in json:
-        reply_text += ("- " + i["name"] + " : " + i["svn_url"] + "\n")
+    repo_data = readCSVfromFile("repo_list.txt")
+    length = len(repo_data)
+    id = str(update.message.chat_id)
+    text = "Here is your list of repo registered in the bot: "
     
-    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
-
-def pull_request_logic(context):
-    url = "https://api.github.com/repos/indocom/pinus-telebot/pulls"
-    response = requests.get(url)
-    json = response.json()
-
-    top_5 = json[0:5]
-    new_pulls = []
-
-    for i in top_5:
-        create_time = datetime.datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
-        time_difference = (datetime.datetime.now() - create_time).total_seconds()
-        #28800 -> conversion from utc to gmt+8
-        # + 5 mins is 29100
-        if time_difference < 29100 :
-            new_pulls.append(i)
-        
-    reply_text = ("Hi, here is the list of new Pull Request for pinus-telebot: \n")
-    for i in new_pulls:
-        reply_text += ("- " + i["title"] + " : " + i["html_url"] + "\n")
-    
-    if len(new_pulls) > 0:
-        context.bot.send_message(chat_id=context.job.context, text=reply_text)
+    for key, value in repo_data.items():
+        if(value[0] != id):
+            continue
+        github_url = value[2]
+        text += github_url
+        text += "\n"
+    context.bot.send_message(chat_id=id, text=text)
 
 def new_pull_request(update, context):
-    text = "Giving you Pull Request updates of pinus-telebot every 5 minutes"
-    context.bot.send_message(chat_id=update.message.chat_id, text=text)
+    repo_data = readCSVfromFile("repo_list.txt")
+    length = len(repo_data)
+    id = str(update.message.chat_id)
 
-    context.job_queue.run_repeating(pull_request_logic, interval = 300, first = 1, context=update.message.chat_id)
+    text = ""
+    for key, value in repo_data.items():
+        if(value[0] != id):
+            continue
+        
+        github_url = value[2][19:]
+        url = "https://api.github.com/repos/" + github_url + "/pulls"
+        try:
+            response = requests.get(url, auth=('user',GITHUB_API_TOKEN))
+            json = response.json()
+        except requests.exceptions.RequestException as e:
+            context.bot.send_message(chat_id = id, text = "An error occured, Pull request may be Incomplete")
+            
+        top_5 = json[0:5]
+        new_pulls = []
+
+        for i in top_5:
+            create_time = datetime.datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+            time_difference = (datetime.datetime.now() - create_time).total_seconds()
+            #28800 -> conversion from utc to gmt+8
+            # + 5 mins is 29100
+            print(time_difference)
+            if time_difference < 29100 :
+                new_pulls.append(i)
+        
+        if(len(new_pulls) == 0):
+            continue
+        text += ("\nREPO NAME: " + value[2][19:] + '\n\n')
+        for i in new_pulls:
+            text += ("- " + i["title"] + " : " + i["html_url"] + "\n\n")
+    
+    if (len(text) > 0):
+        final_text = "Hi, here is the new Pull Requests:\n" + text
+
+    else:
+        final_text = "There is no new Pull Requests"
+    context.bot.send_message(chat_id=id, text=final_text)
+    
 
 def broadcast_pull_request(context):
-    url = "https://api.github.com/repos/indocom/pinus-telebot/pulls"
-    response = requests.get(url)
-    json = response.json()
-
-    top_5 = json[0:5]
-    new_pulls = []
-    for i in top_5:
-        create_time = datetime.datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
-        time_difference = (datetime.datetime.now() - create_time).total_seconds()
-        #28800 -> conversion from utc to gmt+8
-        # + 5 mins is 29100
-        print(time_difference)
-        if time_difference < 29100 :
-            new_pulls.append(i)
-        
-    reply_text = ("Hi, here is the list of new Pull Request for pinus-telebot: \n")
-    for i in new_pulls:
-        reply_text += ("- " + i["title"] + " : " + i["html_url"] + "\n")
+    repo_data = readCSVfromFile("repo_list.txt")
+    length = len(repo_data)
     
-    if len(new_pulls) > 0:
-        for i in chat_ids:
-            context.bot.send_message(chat_id=i, text=reply_text)
-    else:
-        for i in chat_ids:
-            context.bot.send_message(chat_id=i, text="nothing to report")
+    chat_ids = []
 
-def subscribe(update, context):
-    if(update.effective_chat.id in chat_ids):
-        chat_ids.remove(update.effective_chat.id)
-        reply_text = "Successfully Unubscribed"
-    else:
-        chat_ids.append(update.effective_chat.id)
-        reply_text = "Successfully Subscribed"   
-    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+    for key, value in repo_data.items():
+        if(len(value) == 0):
+            continue
+        if(value[0] not in chat_ids):
+            chat_ids.append(value[0])
+    
+    for id in chat_ids:
+        text = ""
+        for key, value in repo_data.items():
+            if(value[0] != id):
+                continue
+
+            url = "https://api.github.com/repos/" + value[2][19:] + "/pulls"
+            print(url)
+            try:
+                response = requests.get(url, auth=('user',GITHUB_API_TOKEN))
+                json = response.json()
+            except requests.exceptions.RequestException as e:
+                context.bot.send_message(chat_id = id, text = "An error occured, Pull request may be Incomplete")
+
+            top_5 = json[0:5]
+            new_pulls = []
+
+            for i in top_5:
+                create_time = datetime.datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+                time_difference = (datetime.datetime.now() - create_time).total_seconds()
+                #28800 -> conversion from utc to gmt+8
+                # + 5 mins is 29100
+                print(time_difference)
+                if time_difference < 29100 :
+                    new_pulls.append(i)
+            
+            if(len(new_pulls) == 0):
+                continue
+            text += ("\nREPO NAME: " + value[2][19:] + '\n\n')
+            for i in new_pulls:
+                text += ("- " + i["title"] + " : " + i["html_url"] + "\n\n")
+        
+        if (len(text) > 0):
+            final_text = "Hi, here is the new Pull Requests:\n" + text
+            context.bot.send_message(chat_id=id, text=final_text)
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def main():
+def connectCalendar():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -182,29 +215,25 @@ def main():
     f.close()
 
 def getevents(update, context):
-    if __name__ == '__main__':
-        main()
+    connectCalendar()
     f = open("events.txt", "r")
     reply_text = ("Getting the upcoming 10 events: \n")
     for i in f:
         reply_text += (i)
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
-getevents_handler = CommandHandler('getevents', getevents)
-dispatcher.add_handler(getevents_handler)
 
 def reminder(context):
-    if __name__ == '__main__':
-        main()
+    connectCalendar()
     f = open("events.txt", "r")
+    text = ''
     for i in f:
         startime = datetime.datetime.strptime(i[0:19], "%Y-%m-%dT%H:%M:%S")
         minute = (startime - datetime.datetime.now()).total_seconds()/60
-        a = i
-        break
-    text = ''
-    if 59 <= minute < 61:
-        text = "Reminder: You have an event in 1 hour. \n"
-        text += a
+        if 59 <= minute < 61:
+            a = i
+            text = "Reminder: You have an event in 1 hour. \n"
+            text += a
+            break
     if len(text) > 2:
         context.bot.send_message(chat_id=context.job.context, text=text)
 
@@ -212,32 +241,142 @@ def remindme(update, context):
     reply = 'Reminder is on.'
     context.bot.send_message(chat_id=update.message.chat_id, text=reply)
     context.job_queue.run_repeating(reminder, interval = 120, first = 1, context=update.message.chat_id)
-remindme_handler = CommandHandler('remindme', remindme)
-dispatcher.add_handler(remindme_handler)
+
+def addevent(update, context):
+    try:
+        eventname = context.args[0]
+        eventdate = context.args[1]
+        eventtime = context.args[2]+":00"
+        checkdate = datetime.datetime.strptime(context.args[1], "%Y-%m-%d")
+        checktime = datetime.datetime.strptime(eventtime, "%H:%M:%S")
+        eventdesc = context.args[3]
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /addevent <name> <date> <time> <desc>')
+        update.message.reply_text('ex: /addevent Meeting 2021-09-28 15:30 Pinus')
+    starttime = eventdate+"T"+eventtime
+    endtime = datetime.datetime.strptime(starttime, "%Y-%m-%dT%H:%M:%S")+datetime.timedelta(hours = 1)
+    endtime = endtime.strftime("%Y-%m-%dT%H:%M:%S")
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    service = build('calendar', 'v3', credentials=creds)
+    # Call the Calendar API
+    event = {
+    'summary': eventname,
+    'description': eventdesc,
+    'start': {
+      'dateTime': starttime,
+     'timeZone': '+08:00',
+    },
+    'end': {
+      'dateTime': endtime,
+      'timeZone': '+08:00',
+     },
+     #'recurrence': [
+     # 'RRULE:FREQ=DAILY;COUNT=2'
+     #],
+     'reminders': {
+     'useDefault': False,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+         {'method': 'popup', 'minutes': 10},
+       ],
+     },
+    }
+    event = service.events().insert(calendarId='tech.pinusonline@gmail.com', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+    update.message.reply_text('Event created: %s' % (event.get('htmlLink')))
 
 def status(update, context):
-    if(update.effective_chat.id in chat_ids):
-        reply_text = "Subscribed"
+    repo_data = readCSVfromFile("repo_list.txt")
+    length = len(repo_data)
+
+    subscribed_repo = []
+    text = "Hi, here is the list of your subscribed repositories:\n"
+
+    for key, value in repo_data.items():
+        if(len(value) > 0 and value[0] == str(update.message.chat_id)):
+            subscribed_repo.append(value[2])
+
+    if(len(subscribed_repo) > 0):
+        for i in subscribed_repo:
+            text += "- " + i + "\n"
+        update.message.reply_text(text)
     else:
-        reply_text = "Not Subscribed"   
-    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+        update.message.reply_text("You have not subscribed to any repositories. Use /help for more information")
 
 def add_repo(update, context):
     repo_data = readCSVfromFile("repo_list.txt")
     length = len(repo_data)
+    
     try:
         # args[0] should contain the time for the timer in seconds
         new_github_url = context.args[0]
         new_chat_id = str(update.message.chat_id)
-        new_owner_name = update.message.from_user.username
-        repo_data[length] = [new_chat_id, new_owner_name, new_github_url]
-
-        text = 'Successfully added new repo'
-        writeToCSV("repo_list.txt", repo_data)
-        update.message.reply_text(text + str(repo_data))
 
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add_repo <repo_link>')
+
+    #Catch duplicate entries
+    dupes = False
+    id = str(update.message.chat_id)
+    text = ""
+    for key, value in repo_data.items():
+        if(value[0] == id and value[2] == new_github_url):
+            dupes = True
+            
+    if(dupes) :
+        update.message.reply_text("The repo has already been registered !")
+        return
+
+    new_owner_name = update.message.from_user.username
+    repo_data[length] = [new_chat_id, new_owner_name, new_github_url]
+
+    text = 'Successfully added new repo'
+    fieldname = ['chat_id', 'owner_name', 'repo_url']
+    writeToCSV("repo_list.txt", repo_data, fieldname)
+    update.message.reply_text(text + str(repo_data))
+
+def remove_repo(update, context):
+    repo_data = readCSVfromFile("repo_list.txt")
+    length = len(repo_data)
+    id = str(update.message.chat_id)
+    try:
+        # args[0] should contain the time for the timer in seconds
+        to_be_deleted_github_url = context.args[0]
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /remove_repo <repo_link>')
+        return
+    delete = False
+    new_repo_data = dict(repo_data)
+    for key, value in repo_data.items():
+        if(value[0] == id and value[2] == to_be_deleted_github_url):
+            delete = True
+            new_repo_data.pop(key)
+    print(new_repo_data)
+    fieldname = ['chat_id', 'owner_name', 'repo_url']
+    writeToCSV('repo_list.txt', new_repo_data, fieldname)        
+    print("kawaii")
+    print(to_be_deleted_github_url)
+    if(delete) : 
+        update.message.reply_text('Deleted Successfully !')
+    else :
+        update.message.reply_text('Repo not found')
 
 #job queues
 job = updater.job_queue.run_repeating(broadcast_pull_request, interval=300, first=1)
@@ -249,8 +388,13 @@ help_handler = CommandHandler('help', help_func)
 repo_list_handler = CommandHandler('repo', repo_list)
 new_pull_request_handler = CommandHandler('new_pull_request', new_pull_request)
 add_repo_handler = CommandHandler('add_repo', add_repo)
+remove_repo_handler = CommandHandler('remove_repo', remove_repo)
+getevents_handler = CommandHandler('getevents', getevents)
+remindme_handler = CommandHandler('remindme', remindme)
+addevent_handler = CommandHandler('addevent', addevent)
 
-subscribe_handler = CommandHandler('subscribe', subscribe)
+
+
 status_handler = CommandHandler('status', status)
 
 #List of Message Handlers
@@ -264,9 +408,11 @@ dispatcher.add_handler(help_handler)
 dispatcher.add_handler(repo_list_handler)
 dispatcher.add_handler(new_pull_request_handler)
 dispatcher.add_handler(add_repo_handler)
-dispatcher.add_handler(subscribe_handler)
 dispatcher.add_handler(status_handler)
-
+dispatcher.add_handler(remove_repo_handler)
+dispatcher.add_handler(getevents_handler)
+dispatcher.add_handler(remindme_handler)
+dispatcher.add_handler(addevent_handler)
 dispatcher.add_handler(unknown_handler)
 
 # updater.start_webhook(listen="0.0.0.0",
